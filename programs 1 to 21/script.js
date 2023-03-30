780c0feff6bd39a528965289d80d5b61f3b7a250
ibtasam116 <ibtasam116@gmail.com>
2023-03-30 21:06:07 +0500

    new commit

--- programs 1 to 21/script.js
+++ programs 1 to 21/script.js
@@ -17,6 +17,30 @@ function maxMin() {
 }
 
 
+function maxMinBtw3() {
+    var num1 = document.getElementById("input3").value;
+    var num2 = document.getElementById("input4").value;
+    var num3 = document.getElementById("input5").value;
+
+    if (num1 > num2) {
+        if (num1 > num3) {
+            document.getElementById("maxOutputbtw3").innerHTML = num1 + " is maximum";
+        }
+        else{
+            document.getElementById("maxOutputbtw3").innerHTML = num3 + " is maximum";
+        }
+    }
+    else{
+        if (num2 > num3) {
+            document.getElementById("maxOutputbtw3").innerHTML = num2 + " is maximum";
+        }
+        else{
+            document.getElementById("maxOutputbtw3").innerHTML = num3 + " is maximum";
+        }
+    }
+}
+
+
 
 
 
