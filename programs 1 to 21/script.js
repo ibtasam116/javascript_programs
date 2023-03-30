0d993dca2f58669c77b46063552e5f4f4d62e2f6
ibtasam116 <ibtasam116@gmail.com>
2023-03-30 15:40:36 +0500

    functions used

---
+++ programs 1 to 21/script.js
@@ -0,0 +1,23 @@
+
+
+
+function maxMin(){
+    var num1 = document.getElementById("input1").value;
+
+    alert(num1);
+
+    autoClearInput();
+}
+
+
+
+
+
+
+
+
+
+function autoClearInput(){
+    document.getElementById("input1").value = "";
+}
+
