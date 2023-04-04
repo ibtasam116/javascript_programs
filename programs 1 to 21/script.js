174c03caf308e144c5c486cc06ab550b5adba49f
ibtasam116 <ibtasam116@gmail.com>
2023-04-04 22:49:40 +0500

    Today Last Commit

--- programs 1 to 21/script.js
+++ programs 1 to 21/script.js
@@ -603,9 +603,37 @@ function perGraFiveSubjects() {
     // document.getElementById('perGraFiveSubjects').innerHTML = `Total marks obtained: ${totalMarks}/500 <br> Percentage obtained: ${percentage.toFixed(2)}% <br> Grade: ${grade}`
 }
 
+// !====================== 20 ======================!
+// Write a js program to input basic salary of an employee and calculate its Gross salary according to following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+function calGrossSalary() {
+    let basicSalary = parseFloat(document.getElementById('basicSalary').value);
 
+    if (!basicSalary) {
+        document.getElementById('calGrossSalary').innerHTML = `Please enter basic salary of an employee`;
+        alert(`Please enter basic salary of an employee`);
+        return;
+    }
 
+    let HRA, DA;
 
+    if (basicSalary <= 10000) {
+        HRA = 0.2 * basicSalary;
+        DA = 0.8 * basicSalary;
+    }
+    else if (basicSalary <= 20000) {
+        HRA = 0.25 * basicSalary;
+        DA = 0.9 * basicSalary;
+    }
+    else {
+        HRA = 0.3 * basicSalary;
+        DA = 0.95 * basicSalary;
+    }
+
+    let grossSalary = basicSalary + HRA + DA;
+}
 
 
 
