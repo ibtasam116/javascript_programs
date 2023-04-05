74727246d16eab3de6b81d9a10895fdd3d452568
ibtasam116 <ibtasam116@gmail.com>
2023-04-05 11:11:37 +0500

    almost complete programs

--- programs 1 to 21/script.js
+++ programs 1 to 21/script.js
@@ -637,8 +637,36 @@ function calGrossSalary() {
     document.getElementById("calGrossSalary").innerHTML = "Basic Salary: " + basicSalary + "<br> HRA: " + HRA + " <br> DA: " + DA + "<br> Gross Salary: " + grossSalary;
 }
 
+// !====================== 21 ======================!
+// Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For unit above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill
+function electricityBill() {
+    let unitCharges = parseFloat(document.getElementById('unitCharges').value);
 
+    let totalBill;
 
+    if (unitCharges <= 50) {
+        totalBill = unitCharges * 0.5;
+    }
+    else if (unitCharges <= 150) {
+        totalBill = 25 + (unitCharges - 50) * 0.75;
+    }
+    else if (unitCharges <= 250) {
+        totalBill = 100 + (unitCharges - 150) * 1.20;
+    }
+    else {
+        totalBill = 220 + (unitCharges - 250) * 1.50;
+    }
+
+    // add 20% surcharge to the bill
+    totalBill *= 1.2;
+
+    document.getElementById('electricityBill').innerHTML = "Total Electricity Bill: <span style='color: green; font-size: 18px;'>" + totalBill + "</span>";
+}
 
 
 
