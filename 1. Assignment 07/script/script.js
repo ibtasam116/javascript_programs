f06a6dd7f0b123d08a26a1474b02260e26a83be4
ibtasam116 <ibtasam116@gmail.com>
2023-05-24 12:58:26 +0500

    Today COmmit

--- 1. Assignment 07/script/script.js
+++ 1. Assignment 07/script/script.js
@@ -1,55 +1,71 @@
+Meter// function checkPasswordStrength() {
+//     var password = inputFieldValues("password");
+//     var strengthMeter = document.getElementById('strength-meter');
+//     var strengthText = document.getElementById('strength-text');
+
+//     // Reset the strength meter and text
+//     strengthMeter.value = 0;
+//     strengthText.textContent = '';
+
+//     // console.log(strengthMeter);
+//     // console.log(strengthText);
+
+//     var strength = 0;
+
+//     if (password.length >= 8) {
+//         strength += 1
+//     }
+
+//     if (/[a-z]/.test(password) && /[A-Z]/.test(password)) {
+//         strength += 1
+//     }
+
+//     if (/\d/.test(password)) {
+//         strength += 1
+//     }
+
+//     if (/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
+//         strength += 1
+//     }
+
+//     // Update the strength meter and text
+//     strengthMeter.value = strength;
+
+//     switch (strength) {
+//         case 0:
+//             strengthText.textContent = 'Weak';
+//             break;
+//         case 1:
+//             strengthText.textContent = 'Moderate';
+//             break;
+//         case 2:
+//             strengthText.textContent = 'Strong';
+//             break;
+//         case 3:
+//             strengthText.textContent = 'Very Strong';
+//             break;
+//         case 4:
+//             strengthText.textContent = 'Extremely Strong';
+//             break;
+//     }
+// }
+
+
 function checkPasswordStrength() {
-    var password = inputFieldValues("password");
-    var strengthMeter = document.getElementById('strength-meter');
-    var strengthText = document.getElementById('strength-text');
+    var password = inputFieldValues('password');
+    // console.log(password);
+    var strengthMeter = document.getElementById("strength-meter");
+    var strengthText = document.getElementById("strength-text");
 
-    // Reset the strength meter and text
     strengthMeter.value = 0;
-    strengthText.textContent = '';
-
     // console.log(strengthMeter);
+    strengthText.textContent = ""
     // console.log(strengthText);
 
-    var strength = 0;
+    document.getElementBy
 
-    if (password.length >= 8) {
-        strength += 1
-    }
 
-    if (/[a-z]/.test(password) && /[A-Z]/.test(password)) {
-        strength += 1
-    }
-
-    if (/\d/.test(password)) {
-        strength += 1
-    }
-
-    if (/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
-        strength += 1
-    }
-
-    // Update the strength meter and text
-    strengthMeter.value = strength;
-
-    switch (strength) {
-        case 0:
-            strengthText.textContent = 'Weak';
-            break;
-        case 1:
-            strengthText.textContent = 'Moderate';
-            break;
-        case 2:
-            strengthText.textContent = 'Strong';
-            break;
-        case 3:
-            strengthText.textContent = 'Very Strong';
-            break;
-        case 4:
-            strengthText.textContent = 'Extremely Strong';
-            break;
-    }
 }
-
 
 
 function checkPalindrome() {
@@ -62,13 +78,18 @@ function checkPalindrome() {
     // Reverse the string
     var reversedInput = formattedInput.split('').reverse().join('');
 
+    if (!input) {
+        result.textContent = 'Please Enter Number'
+        return;
+    }
+
     // Check if the reversed string is equal to the original string
     if (formattedInput === reversedInput) {
-      result.textContent = 'Palindrome';
+        result.textContent = 'Palindrome';
     } else {
-      result.textContent = 'Not a Palindrome';
+        result.textContent = 'Not a Palindrome';
     }
-  }
+}
 
 
 
